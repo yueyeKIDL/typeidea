29253b02f154c3347e05c9a75374f13dd38def1b
yueyeKIDL <ace1412kid@163.com>
2019-09-16 11:48:47 +0800

    新增本地jquery文件，解决django-debug-toolbar加载缓慢问题

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -7,3 +7,16 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1', ]
+
+
+DEBUG_TOOLBAR_CONFIG = {'JQUERY_URL': '/static/js/jquery-3.3.1.js'}
